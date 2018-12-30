4870c3df942333a0d0dbdded4fc1c0b01e6fc549
Jef Spaleta <jef@sensu.io>
2018-12-30 00:58:43 -0900

    fixes for golint run

--- particle_variable_metric_check/main.go
+++ particle_variable_metric_check/main.go
@@ -12,28 +12,29 @@ import (
 )
 
 var (
-	access_token string
-	device_id    string
-	product_id   string
-	metric_name  string
+	accessToken string
+	deviceID    string
+	productID   string
+	metricName  string
 	variable     string
 	timestamp    string
-	timeout      int = 60
+	timeout      = 60
 	verbose      bool
 )
 
-type CoreInfo struct {
-	Name              string
+type coreInfo struct {
 	DeviceID          string
+	ProductID         int    `json:"product_id"`
 	Connected         bool
-	Last_handshake_at string
-	Last_app          string
+	LastHandshakeAt   string `json:"last_handshake_at"`
+	LastApp           string `json:"last_app"`
+	LastHeard         string `json:"last_heard"`
 }
-type ParticleVar struct {
+type particleVar struct {
 	Name      string
 	Result    string
 	Timestamp int
-	CoreInfo  CoreInfo
+	CoreInfo  coreInfo
 }
 
 func main() {
@@ -48,12 +49,12 @@ func main() {
 func configureRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "particle_variable_check",
-		Short: "Retrieve Particle variable",
+		Short: "Retrieve Particle variable as graphite metric",
 		Long:  `Retrieve Particle string variable and output in graphite plaintext format`,
 		RunE:  run,
 	}
 
-	cmd.Flags().StringVarP(&device_id,
+	cmd.Flags().StringVarP(&deviceID,
 		"device",
 		"d",
 		"",
@@ -61,7 +62,7 @@ func configureRootCommand() *cobra.Command {
 
 	_ = cmd.MarkFlagRequired("device")
 
-	cmd.Flags().StringVarP(&access_token,
+	cmd.Flags().StringVarP(&accessToken,
 		"access_token",
 		"a",
 		"",
@@ -87,13 +88,13 @@ func configureRootCommand() *cobra.Command {
 		60,
 		"Optional particle Metric Timestamp Timeout (seconds)")
 
-	cmd.Flags().StringVarP(&product_id,
+	cmd.Flags().StringVarP(&productID,
 		"product",
 		"p",
 		"",
 		"Optional Particle Product ID")
 
-	cmd.Flags().StringVarP(&metric_name,
+	cmd.Flags().StringVarP(&metricName,
 		"metric",
 		"m",
 		"",
@@ -113,7 +114,7 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("invalid argument(s) received")
 	}
 
-	var output ParticleVar
+	var output particleVar
 	if timeout < 0 {
 		timeout = 0
 	}
@@ -122,66 +123,74 @@ func run(cmd *cobra.Command, args []string) error {
         if (err != nil) {
           return nil
         }
-	if timeout == 0 || (int(t) - int(output.Timestamp) ) <= int(timeout) {
-		if metric_name == "" {
-			metric_name, err = os.Hostname()
-			metric_name += "." + output.Name
+	if timestamp == "" || timeout == 0 || (int(t) - int(output.Timestamp) ) <= int(timeout) {
+		if metricName == "" {
+			metricName, err = os.Hostname()
+			metricName += "." + output.Name
 		}
-		fmt.Printf("%s %s %d\n", metric_name, output.Result, output.Timestamp)
+		fmt.Printf("%s %s %d\n", metricName, output.Result, output.Timestamp)
 	} else {
-		err = fmt.Errorf("Stale Variable Measurement: %d - %d = %d\n",int(t),output.Timestamp, int(t)-output.Timestamp)
+		err = fmt.Errorf("stale variable measurement: %d - %d = %d",int(t),output.Timestamp, int(t)-output.Timestamp)
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 	return err
 }
 
-func particleDeviceVariable(output *ParticleVar) error {
-	var baseUrlStr string
-	var variableUrlStr string
-	baseUrlStr = "https://api.particle.io/v1/"
-	if product_id != "" {
-		baseUrlStr += "products/" + product_id + "/"
-	}
-	variableUrlStr = baseUrlStr + "devices/" + device_id + "/" + variable + "?access_token=" + access_token
-	if verbose {
-		fmt.Printf("Device:%s Token:%s Variable:%s\n", device_id, access_token, variable)
-		if product_id != "" {
-			fmt.Printf("  Product:%s\n", product_id)
-		}
-		fmt.Printf("  Url:%s\n", variableUrlStr)
-	}
-	body, err := MakeRequest(variableUrlStr)
-        if (err != nil) {
-	  fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	  os.Exit(1)
-        }
+func particleDeviceVariable(output *particleVar) error {
+	var baseURLStr string
+	var variableURLStr string
+        var err error
+	var tout particleVar
+	var body []byte
 
-	json.Unmarshal([]byte(body), &output)
+	baseURLStr = "https://api.particle.io/v1/"
+	if productID != "" {
+		baseURLStr += "products/" + productID + "/"
+	}
+	variableURLStr = baseURLStr + "devices/" + deviceID + "/" + variable + "?access_token=" + accessToken
+	if verbose {
+		fmt.Printf("Device:%s Token:%s Variable:%s\n", deviceID, accessToken, variable)
+		if productID != "" {
+			fmt.Printf("  Product:%s\n", productID)
+		}
+		fmt.Printf("  Url:%s\n", variableURLStr)
+	}
+
 	if timestamp != "" {
-		var timestampUrlStr string
-		timestampUrlStr = baseUrlStr + "devices/" + device_id + "/" + timestamp + "?access_token=" + access_token
-		var tout ParticleVar
+		var timestampURLStr string
+		timestampURLStr = baseURLStr + "devices/" + deviceID + "/" + timestamp + "?access_token=" + accessToken
 		var tbody []byte
-		tbody, err = MakeRequest(timestampUrlStr)
+		tbody, err = makeRequest(timestampURLStr)
         	if (err != nil) {
 	  		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	  		os.Exit(1)
+	  		os.Exit(2)
         	}
 		if verbose {
 			fmt.Printf("Timestamp Response: %s\n", tbody)
 		}
 		json.Unmarshal([]byte(tbody), &tout)
-		output.Timestamp, err = strconv.Atoi(tout.Result)
 	}
+
+	body, err = makeRequest(variableURLStr)
+        if (err != nil) {
+	  fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	  os.Exit(2)
+        }
+	json.Unmarshal([]byte(body), &output)
+
+	if timestamp == "" { 
+        	output.Timestamp = int(time.Now().Unix()) 
+        } else { output.Timestamp, err = strconv.Atoi(tout.Result) }
 	if verbose {
 		fmt.Printf("Response: %s\n", body)
 		fmt.Printf("Var:%s Val:%s Timestamp: %d\n", output.Name, output.Result, output.Timestamp)
+		fmt.Printf("DeviceID:%v LastHeard:%v LastHandshakeAt:%v ProductID: %v\n", output.CoreInfo.DeviceID,output.CoreInfo.LastHeard,output.CoreInfo.LastHandshakeAt,output.CoreInfo.ProductID)
 	}
 	return err
 }
 
-func MakeRequest(urlStr string) ([]byte, error) {
+func makeRequest(urlStr string) ([]byte, error) {
 	resp, err := http.Get(urlStr)
 	if err != nil {
 	  	fmt.Fprintf(os.Stderr, "error: %v\n", err)
