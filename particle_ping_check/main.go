72e128d3f95b9efcdf27b21fa46c1ab4fab86445
Jef Spaleta <jef@sensu.io>
2018-12-30 00:11:06 -0900

    working particle device ping

--- particle_ping_check/main.go
+++ particle_ping_check/main.go
@@ -3,40 +3,69 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/sensu/sensu-go/types"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"net/http"
+	//"net/url"
+	"os"
+	//"strings"
 )
 
 var (
-	foo   string
-	stdin *os.File
+	access_token string
+	device_id    string
+	product_id   string
+	verbose      bool
 )
+
+type Ping struct {
+	Online bool
+	Ok     bool
+}
 
 func main() {
 	rootCmd := configureRootCommand()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
+	} else {
 	}
 }
 
 func configureRootCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "particle_ping_check",
-		Short: "Ping Particle Device",
+		Short: "Ping Particle device",
+		Long:  `Ping Particle device and check to see if its online`,
 		RunE:  run,
 	}
 
-	cmd.Flags().StringVarP(&foo,
-		"foo",
-		"f",
+	cmd.Flags().StringVarP(&device_id,
+		"device",
+		"d",
 		"",
-		"example")
+		"Particle Device ID")
 
-	_ = cmd.MarkFlagRequired("foo")
+	_ = cmd.MarkFlagRequired("device")
+
+	cmd.Flags().StringVarP(&access_token,
+		"access_token",
+		"a",
+		"",
+		"Particle Access Token")
+	_ = cmd.MarkFlagRequired("access_token")
+
+	cmd.Flags().StringVarP(&product_id,
+		"product",
+		"p",
+		"",
+		"Optional Particle Product ID")
+
+	cmd.Flags().BoolVar(&verbose,
+		"verbose",
+		false,
+		"Enable verbose output")
 
 	return cmd
 }
@@ -47,36 +76,64 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("invalid argument(s) received")
 	}
 
-	if stdin == nil {
-		stdin = os.Stdin
-	}
-
-	eventJSON, err := ioutil.ReadAll(stdin)
+	var output Ping
+	err := particlePing(&output)
 	if err != nil {
-		return fmt.Errorf("failed to read stdin: %s", err)
+		return err
 	}
-
-	event := &types.Event{}
-	err = json.Unmarshal(eventJSON, event)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal stdin data: %s", err)
-	}
-
-	if err = event.Validate(); err != nil {
-		return fmt.Errorf("failed to validate event: %s", err)
-	}
-
-	if !event.HasCheck() {
-		return fmt.Errorf("event does not contain check")
-	}
-
-	return exampleAction(event)
+	fmt.Printf("Device: %s Online: %v Ok: %v\n", device_id, output.Online,output.Ok)
+        if ( !( output.Online && output.Ok )) { os.Exit(2) }
+	return err
 }
 
-func exampleAction(event *types.Event) error {
-	fmt.Printf("hello world: %s\n", foo)
+func particlePing(output *Ping) error {
+	var baseUrlStr string
+	baseUrlStr = "https://api.particle.io/v1/"
+	if product_id != "" {
+		baseUrlStr += "products/" + product_id + "/"
+	}
+	baseUrlStr += "devices/" + device_id + "/ping"
+	if verbose {
+		fmt.Printf("  Url:%s\n", baseUrlStr)
+	}
+	body, err := MakeRequest(baseUrlStr, access_token)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 
-	fmt.Printf("Event Key: %s-%s\n", event.Entity.Name, event.Check.Name)
+	json.Unmarshal([]byte(body), &output)
+	if verbose {
+		fmt.Printf("Response: %s\n", body)
+	}
+	return err
+}
 
-	return nil
+func MakeRequest(urlStr string, access_token string) ([]byte, error) {
+        data := "Bearer "+access_token
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, urlStr, nil)
+	req.Header.Add("Authorization",data)
+	fmt.Fprintf(os.Stderr, "Header: %v\n", req.Header)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//hdr := resp.Header
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("Failed Request %s StatusCode: %v\n%v", urlStr, resp.StatusCode,string(contents))
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	return contents, err
 }
